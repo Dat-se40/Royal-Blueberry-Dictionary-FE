daedf24e322ae4897fa7250964a2f10be9d82a4a
DatOnTheRoad <tandatthtt@gmail.com>
2026-03-17 16:59:13 +0700

    feat: resigter dependencies for database, repository

--- Royal Blueberry Dictionary/App.xaml.cs
+++ Royal Blueberry Dictionary/App.xaml.cs
@@ -3,6 +3,8 @@ using System.Configuration;
 using System.Data;
 using System.Windows;
 using System.Windows.Media.Animation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.WebSockets;
 
 namespace Royal_Blueberry_Dictionary
 {
@@ -11,13 +13,34 @@ namespace Royal_Blueberry_Dictionary
     /// </summary>
     public partial class App : Application
     {
-        public App() 
+        public static ServiceProvider serviceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
         {
-            using (var db = new AppDbContext())
+            base.OnStartup(e);
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<AppDbContext>();
+
+            // Repositories
+            serviceCollection.AddScoped<Repository.Interface.IWordEntryRepository, Repository.Implement.WordEntryRepository>();
+
+            serviceProvider = serviceCollection.BuildServiceProvider();
+
+            using (var scope = serviceProvider.CreateScope())
             {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
                 db.Database.EnsureCreated();
-            }   
+            }
         }
-    }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            if (serviceProvider != null)
+            {
+                serviceProvider.Dispose();
+            }
+        }
+    } 
 
 }
